5cbc363de8a041489e04c8588e9cc6d5e42d8b6e
AAZAD001 <73563976+AAZAD001@users.noreply.github.com>
2024-10-08 19:45:45 -0700

    Initialize Deleted Sample Code files and gave fxid to guest spinner and total rate label

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.hotelproject2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.hotelproject2 to javafx.fxml;
-    exports com.example.hotelproject2;
-}

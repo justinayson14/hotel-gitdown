a0e6a2e7019519f35f9c647fd49ae82211ea319b
Justin Ayson <121000453+justinayson14@users.noreply.github.com>
2024-10-23 19:45:41 -0700

    Configured class variables

--- src/main/java/com/example/hotelproject2/CreateDoc.java
+++ src/main/java/com/example/hotelproject2/CreateDoc.java
@@ -14,16 +14,16 @@ import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 public class CreateDoc {
-    private static ConnectionString connectionString = new ConnectionString(System.getenv("apiKey"));
-    private static CodecRegistry pojoCodecRegistry = fromProviders(PojoCodecProvider.builder().automatic(true).build());
-    private static CodecRegistry codecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(), pojoCodecRegistry);
-    private static MongoClientSettings clientSettings = MongoClientSettings.builder()
+    private static final ConnectionString connectionString = new ConnectionString(System.getenv("apiKey"));
+    private static final CodecRegistry pojoCodecRegistry = fromProviders(PojoCodecProvider.builder().automatic(true).build());
+    private static final CodecRegistry codecRegistry = fromRegistries(MongoClientSettings.getDefaultCodecRegistry(), pojoCodecRegistry);
+    private static final MongoClientSettings clientSettings = MongoClientSettings.builder()
             .applyConnectionString(connectionString)
             .codecRegistry(codecRegistry)
             .build();
 
-    public static void main(String args[]) {
-        Customers person1 = new Customers("123123", "Joe", 10);
+    public static void main(String[] args) {
+        Customers person1 = new Customers("1233221321", "Joe", 10);
         try (MongoClient mongoClient = MongoClients.create(clientSettings)) {
             MongoDatabase db = mongoClient.getDatabase("hotel-clients");
             MongoCollection<Customers> customers = db.getCollection("clients", Customers.class);

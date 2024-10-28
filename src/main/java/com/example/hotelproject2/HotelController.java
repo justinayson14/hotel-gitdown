92a6b35d037ea368c64f39ad8d83fcbfe0f9bb31
Justin Ayson <121000453+justinayson14@users.noreply.github.com>
2024-10-28 00:04:41 -0700

    Created controller for room CRUD ops

--- src/main/java/com/example/hotelproject2/HotelController.java
+++ src/main/java/com/example/hotelproject2/HotelController.java
@@ -1,4 +1,33 @@
 package com.example.hotelproject2;
 
+import com.example.hotelproject2.models.DeluxeRoom;
+import com.example.hotelproject2.models.PresRoom;
+import com.example.hotelproject2.models.Room;
+import com.example.hotelproject2.models.StandardRoom;
+
 public class HotelController {
+
+    public void createRoom (String roomType, int roomNum) {
+        Room room;
+        switch (roomType) {
+            case "Standard":
+                room = new StandardRoom(roomNum);
+                break;
+            case "Deluxe":
+                room = new DeluxeRoom(roomNum);
+                break;
+            case "Presidential":
+                room = new PresRoom(roomNum);
+                break;
+            default:
+                room = null;
+                System.out.println("INVALID Room type!!!");
+                System.exit(1);
+        }
+
+        // insert new room to database
+        MongoOps.insertSingle(room);
+    }
+
+
 }

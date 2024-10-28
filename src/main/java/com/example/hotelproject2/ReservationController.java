8af508fa09fcbc246f7d740210889056e01d2934
Justin Ayson <121000453+justinayson14@users.noreply.github.com>
2024-10-28 15:20:50 -0700

    Added checkIn/Out dates to booking creation

--- src/main/java/com/example/hotelproject2/ReservationController.java
+++ src/main/java/com/example/hotelproject2/ReservationController.java
@@ -1,7 +1,6 @@
 package com.example.hotelproject2;
 
 import com.example.hotelproject2.models.Booking;
-import com.example.hotelproject2.models.Customers;
 import com.example.hotelproject2.models.Payment;
 import com.example.hotelproject2.models.Room;
 
@@ -9,7 +8,6 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
 public class ReservationController {
-
     private double calculateTotalCost (String checkIn, String checkOut, double cost) {
         LocalDate start = LocalDate.parse(checkIn);
         LocalDate end = LocalDate.parse(checkOut);
@@ -27,18 +25,20 @@ public class ReservationController {
         return pay;
     }
 
-    public void createReservation(Customers customer, String checkIn, String checkOut, String roomType, String name, String cardNum, String cardCVC, String cardExp, String phoneNum) {
+    public void createReservation(String customerId, String checkIn, String checkOut, String roomType, String name, String cardNum, String cardCVC, String cardExp, String phoneNum) {
         Booking reservation = new Booking();
         Room room = MongoOps.queryAvailRoomByType(roomType);
-        reservation.setCustomerId(customer.getId());
+        reservation.setCustomerId(customerId);
         reservation.setRoomId(room.getId());
-        reservation.setRoomClass(room.getClass());
+        reservation.setRoomType(room.getRoomType());
+        reservation.setCheckInDate(checkIn);
+        reservation.setCheckOutDate(checkOut);
         reservation.setTotalCost(calculateTotalCost(checkIn, checkOut, room.getPrice()));
         reservation.setPayment(createPayment(name, cardNum, cardCVC, cardExp, phoneNum));
         room.setOccupied(true);
 
         MongoOps.insertSingle(reservation);
-        MongoOps.checkInRoom(room.getClass(), room.getId());
+        MongoOps.checkInRoom(room.getRoomType(), room.getId());
     }
 
     public void endReservation(String customerName) {

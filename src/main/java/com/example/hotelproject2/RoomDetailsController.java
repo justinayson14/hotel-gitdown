d15c32486237fb60778d16f525692377e6e042c1
Justin Ayson <121000453+justinayson14@users.noreply.github.com>
2024-11-26 21:16:06 -0800

    Added methods to calculate total cost & fixed method to payment scene to create Booking object

--- src/main/java/com/example/hotelproject2/RoomDetailsController.java
+++ src/main/java/com/example/hotelproject2/RoomDetailsController.java
@@ -1,7 +1,5 @@
 package com.example.hotelproject2;
-import com.example.hotelproject2.models.DeluxeRoom;
-import com.example.hotelproject2.models.PresRoom;
-import com.example.hotelproject2.models.StandardRoom;
+import com.example.hotelproject2.models.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -17,78 +15,73 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ResourceBundle;
-
-import com.example.hotelproject2.models.Customers;
 
 /**
  Room Details Controller Class*
  */
 public class RoomDetailsController {
-    /**
-     Initializing Variables*
-     */
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
-    private Customers customer;
-
-
     @FXML
     public ChoiceBox<String> roomTypeChoiceBox;
-
-    private String[] roomTypes = {"Standard", "Deluxe", "Presidential"};
-
-
     @FXML
     private Label roomDescText;
     @FXML
     private Label bedNumText;
     @FXML
     private Label bathNumText;
-
-
-
+    @FXML
+    private Label roomCostText;
     @FXML
     private DatePicker startDatePicker;
-
     @FXML
     private DatePicker endDatePicker;
+    @FXML
+    private Label totalCostText;
+
+    private final String[] roomTypes = {"Standard", "Deluxe", "Presidential"};
+    private Customers customer;
+    private double roomCost;
+    private String roomType;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private double totalCost;
 
     /**
-    New Standard Room, New Deluxe Room and New Presidential Room class objects
-     *
+     * This method is used to pass the customer data
+     * from another controller to this controller for later use.
+     * @param customer Object containing details about customer
      */
-
-    StandardRoom newStandard = new StandardRoom();
-    DeluxeRoom newDeluxe = new DeluxeRoom();
-    PresRoom newPres = new PresRoom();
-
-   /* private double calculateTotalCost(String startDatePicker, String endDatePicker, double cost) {
-        LocalDate start = LocalDate.parse(startDatePicker);
-        LocalDate end = LocalDate.parse(endDatePicker);
-        long diffInDays = ChronoUnit.DAYS.between(start, end);
-        return (double)diffInDays * cost;
-    } */
+    public void getCustomer(Customers customer) {
+        this.customer = customer;
+    }
 
     /**
-
      Creates the interaction for the Choice Box*
      */
-
     @FXML
     public void initialize() {
         roomTypeChoiceBox.getItems().addAll(roomTypes);
         roomTypeChoiceBox.setOnAction(this::getRoomType);
     }
 
-    /**
-     * This method is used to pass the customer data
-     * from another controller to this controller for later use.
-     * @param customer
-     */
-    public void getCustomerData(Customers customer) {
-        this.customer = customer;
+    @FXML
+    private void getStartDate(ActionEvent event) {
+        if(startDatePicker.getValue() != null)
+            startDate = startDatePicker.getValue();
+    }
+
+    @FXML
+    private void calculateTotalCost(ActionEvent event) {
+        endDate = endDatePicker.getValue();
+        long diffInDays = ChronoUnit.DAYS.between(startDate, endDate);
+        if (diffInDays <= 0 && startDatePicker != null)
+            totalCostText.setText("Please pick end date after start date...");
+        else {
+            totalCost = diffInDays*roomCost;
+            totalCostText.setText("$"+totalCost);
+        }
     }
 
     /**
@@ -99,22 +92,28 @@ public class RoomDetailsController {
      * the user's choice.
      */
     private void getRoomType(ActionEvent event) {
-        String pickedRoom = roomTypeChoiceBox.getValue();
-        switch (pickedRoom) {
+        roomType = roomTypeChoiceBox.getValue();
+        switch (roomType) {
             case "Standard" -> {
-                roomDescText.setText(newStandard.getDesc());
-                bedNumText.setText(Integer.toString(newStandard.getNumBeds()));
-                bathNumText.setText(Integer.toString(newStandard.getNumBaths()));
+                roomDescText.setText(StandardRoom.getDesc());
+                bedNumText.setText(Integer.toString(StandardRoom.getNumBeds()));
+                bathNumText.setText(Integer.toString(StandardRoom.getNumBaths()));
+                roomCostText.setText("$"+StandardRoom.getPrice());
+                roomCost = StandardRoom.getPrice();
             }
             case "Deluxe" -> {
-                roomDescText.setText(newDeluxe.getDesc());
-                bedNumText.setText(Integer.toString(newDeluxe.getNumBeds()));
-                bathNumText.setText(Integer.toString(newDeluxe.getNumBaths()));
+                roomDescText.setText(DeluxeRoom.getDesc());
+                bedNumText.setText(Integer.toString(DeluxeRoom.getNumBeds()));
+                bathNumText.setText(Integer.toString(DeluxeRoom.getNumBaths()));
+                roomCostText.setText("$"+DeluxeRoom.getPrice());
+                roomCost = DeluxeRoom.getPrice();
             }
             case "Presidential" -> {
-                roomDescText.setText(newPres.getDesc());
-                bedNumText.setText(Integer.toString(newPres.getNumBeds()));
-                bathNumText.setText(Integer.toString(newPres.getNumBaths()));
+                roomDescText.setText(PresRoom.getDesc());
+                bedNumText.setText(Integer.toString(PresRoom.getNumBeds()));
+                bathNumText.setText(Integer.toString(PresRoom.getNumBaths()));
+                roomCostText.setText("$"+PresRoom.getPrice());
+                roomCost = PresRoom.getPrice();
             }
         }
     }
@@ -124,13 +123,13 @@ public class RoomDetailsController {
      * @param event
      * @throws IOException
      */
-    public void switchToCustomer(ActionEvent event) throws IOException {
+    @FXML
+    private void switchToCustomer(ActionEvent event) throws IOException {
         // sends customer data to database
         System.out.print("\nThe following customer data was received and added to the database: " + customer + "\n");
-        //MongoOps.insertSingle(customer);
         Parent root = FXMLLoader.load(getClass().getResource("CustomerScene.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
@@ -140,20 +139,28 @@ public class RoomDetailsController {
      * @param event
      * @throws IOException
      */
-    public void switchToPaymentMethod(ActionEvent event) throws IOException {
+    @FXML
+    private void switchToPaymentMethod(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("PaymentMethodScene.fxml"));
         Parent root = loader.load();
-
         // creates an instance of the scene's controller to pass customer data to
         PaymentMethodSceneController controller = loader.getController();
-
         // prints the customer data to console and passes it along
         System.out.println("\n---\nPassing along the following customer data: " + customer + "\n---"); // prints to console
-        controller.getCustomerData(customer); // passes it along
-
+        System.out.println(roomType);
+        Room room = MongoOps.queryAvailRoomByType(roomType);
+        System.out.println(room.getClass());
+        Booking booking = new Booking();
+        booking.setCustomerId(customer.getId());
+        booking.setCheckInDate(startDate.toString());
+        booking.setCheckOutDate(endDate.toString());
+        booking.setRoomType(room.getClass().getSimpleName());
+        booking.setRoomId(room.getId());
+        booking.setTotalCost(totalCost);
+        controller.initData(customer, booking); // passes it along
         // switch scene
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }

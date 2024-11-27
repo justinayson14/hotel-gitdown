e0d2d3fcfbbe05eb42cf6b16790064a7c8fd9847
Justin Ayson <121000453+justinayson14@users.noreply.github.com>
2024-11-27 00:43:19 -0800

    Defined methods to display customer's room num

--- src/main/java/com/example/hotelproject2/ThankYouSceneController.java
+++ src/main/java/com/example/hotelproject2/ThankYouSceneController.java
@@ -1,19 +1,27 @@
 package com.example.hotelproject2;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class ThankYouSceneController {
+    @FXML
+    private Text roomNumText;
 
     private Stage stage;
     private Scene scene;
     private Parent root;
+
+    public void setRoomNum(int roomNum) {
+        roomNumText.setText(Integer.toString(roomNum));
+    }
 
     /**
      * Switches to HomeScene when "Home" button is clicked

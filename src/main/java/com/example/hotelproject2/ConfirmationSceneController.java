fffbb56519900c66dd3d914c7f63d53c575d1f11
Justin Ayson <121000453+justinayson14@users.noreply.github.com>
2024-11-26 23:48:55 -0800

    Added method to initialize objects & to display proper text for labels

--- src/main/java/com/example/hotelproject2/ConfirmationSceneController.java
+++ src/main/java/com/example/hotelproject2/ConfirmationSceneController.java
@@ -1,11 +1,13 @@
 package com.example.hotelproject2;
 
+import com.example.hotelproject2.models.Booking;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,26 +15,53 @@ import java.io.IOException;
 import com.example.hotelproject2.models.Customers;
 
 public class ConfirmationSceneController {
+    @FXML
+    private Label custNameText;
+    @FXML
+    private Label roomChoiceText;
+    @FXML
+    private Label startDateText;
+    @FXML
+    private Label endDateText;
+    @FXML
+    private Label phoneText;
+    @FXML
+    private Label totalText;
 
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
-    private Customers customer = new Customers();
+    private Customers customer;
+    private Booking booking;
 
     /**
-     * This method is used to pass the customer data
+     * This method is used to pass the customer data, booking data
      * from another controller to this controller for later use.
-     * @param customer
+     * @param customer Customer object from previous scene
+     * @param booking Booking object from previous scene
      */
-    public void getCustomerData(Customers customer) {
+    public void initData(Customers customer, Booking booking) {
         this.customer = customer;
+        this.booking = booking;
+    }
+
+    public void displayText() {
+        custNameText.setText(customer.getName());
+        roomChoiceText.setText(booking.getRoomType());
+        startDateText.setText(booking.getCheckInDate());
+        endDateText.setText(booking.getCheckOutDate());
+        phoneText.setText(booking.getPayment().getPhoneNum());
+        totalText.setText(booking.getTotalCost().toString());
     }
 
     @FXML
     private void switchToPayment(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("HomeScene.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("PaymentMethodScene.fxml"));
+        Parent root = loader.load();
+
+        PaymentMethodSceneController controller = loader.getController();
+        // Pass back objects to previous scene
+        controller.initData(customer, booking);
+
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
@@ -40,8 +69,8 @@ public class ConfirmationSceneController {
     @FXML
     private void switchToThankYou(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("ThankYouScene.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
         

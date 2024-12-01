2a0797f025e36185d066caccc0789572c79211a2
Justin Ayson <121000453+justinayson14@users.noreply.github.com>
2024-12-01 15:27:54 -0800

    Added method to check out room & return room number of room checked out

--- src/main/java/com/example/hotelproject2/CheckOutController.java
+++ src/main/java/com/example/hotelproject2/CheckOutController.java
@@ -6,35 +6,60 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class CheckOutController {
+    @FXML
+    private TextField firstNameText;
+    @FXML
+    private TextField lastNameText;
+    @FXML
+    private Label errorText;
+    @FXML
+    private Label roomIdText;
 
     private Stage stage;
     private Scene scene;
     private Parent root;
-
-    @FXML
-    public TextField firstName;
-    @FXML
-    public TextField secondName;
-
-
+    private final ReservationController r = new ReservationController();
 
     /**
      * Switches to HomeScene when "Back" button is clicked.
      * @param event
      * @throws IOException
      */
-    public void switchToHomeScene(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("HomeScene.fxml"));
+    @FXML
+    private void switchToHomeScene(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("HomeScene.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    @FXML
+    private void handleSwitch(ActionEvent event) throws IOException {
+        boolean isAllFilled = r.validateFields(firstNameText) && r.validateFields(lastNameText);
+        if(isAllFilled)
+            switchToCheckOutConfirm(event);
+    }
+
+    private boolean checkIfFound(String custId, String roomId) throws IOException {
+        if(custId == null || roomId == null) {
+            errorText.setStyle("-fx-text-fill: red;");
+            if(custId == null)
+                errorText.setText("Customer not found");
+            else
+                errorText.setText("No booking found");
+            return false;
+        }
+        errorText.setText(null);
+        errorText.setStyle(null);
+        return true;
     }
 
     /**
@@ -42,12 +67,24 @@ public class CheckOutController {
      * @param event
      * @throws IOException
      */
-    public void switchToCheckOutConfirmation(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("CheckOutConfirmationScene.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+    private void switchToCheckOutConfirm(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("CheckOutConfirmScene.fxml"));
+        root = loader.load();
+
+        CheckOutController controller = loader.getController();
+        String customerName = firstNameText.getText().toLowerCase() + ' ' + lastNameText.getText().toLowerCase();
+        String customerId = MongoOps.queryCustomerIdByName(customerName);
+        if(customerId == null) {
+            errorText.setText("Customer not found");
+            errorText.setStyle("-fx-text-fill: red;");
+        } else {
+            String roomId = MongoOps.checkOutRoom(customerId);
+            controller.roomIdText.setText(roomId);
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
     }
 
 }

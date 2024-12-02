6f0fcc8106a623f13b1ed33bd88e760eddb66165
Justin Ayson <121000453+justinayson14@users.noreply.github.com>
2024-12-01 22:20:24 -0800

    Added method to switch to guest home scene

--- src/main/java/com/example/hotelproject2/LoginSceneController.java
+++ src/main/java/com/example/hotelproject2/LoginSceneController.java
@@ -1,4 +1,28 @@
 package com.example.hotelproject2;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginSceneController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    private void switchToHomeScene(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("HomeScene.fxml"));
+        root = loader.load();
+        HomeSceneController controller = loader.getController();
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }

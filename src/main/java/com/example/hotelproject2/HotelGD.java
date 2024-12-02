5eb4e2059db2a39f3a797b95e44c9a84a5deb134
Justin Ayson <121000453+justinayson14@users.noreply.github.com>
2024-12-01 22:19:52 -0800

    Changed the first scene to be login scene & non-resizable

--- src/main/java/com/example/hotelproject2/HotelGD.java
+++ src/main/java/com/example/hotelproject2/HotelGD.java
@@ -9,12 +9,15 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HotelGD extends Application {
+    public static Stage stage;
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("HomeScene.fxml"));
+        this.stage = stage;
+        Parent root = FXMLLoader.load(getClass().getResource("LoginScene.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+        stage.setResizable(false);
     }
 
     public static void main(String[] args) {

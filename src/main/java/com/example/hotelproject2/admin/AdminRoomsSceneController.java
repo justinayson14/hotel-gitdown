2da1b14107750a76c1ba5fca5af2c6bced4a5419
Justin Ayson <121000453+justinayson14@users.noreply.github.com>
2024-12-04 15:35:46 -0800

    Initialized table to show all Standard rooms & toggle Standard button

--- src/main/java/com/example/hotelproject2/admin/AdminRoomsSceneController.java
+++ src/main/java/com/example/hotelproject2/admin/AdminRoomsSceneController.java
@@ -2,7 +2,6 @@ package com.example.hotelproject2.admin;
 
 import com.example.hotelproject2.MongoOps;
 import com.example.hotelproject2.models.Room;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -11,6 +10,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -25,10 +26,46 @@ public class AdminRoomsSceneController {
     private List<Room> rooms;
 
     @FXML
-    private void initialize() {
-        rooms = MongoOps.queryAllRoomsByType("Standard");
-        ObservableList<Room> observable = FXCollections.observableList(rooms);
+    private TableColumn<Room, String> roomIdCol;
+    @FXML
+    private TableColumn<Room, Integer> roomNumCol;
+    @FXML
+    private TableColumn<Room, Boolean> roomOccCol;
+    @FXML
+    private TableView<Room> table;
+    @FXML
+    private ToggleButton stdBtn;
+    @FXML
+    private ToggleButton deluxeBtn;
+    @FXML
+    private ToggleButton presBtn;
+    @FXML
+    private ChoiceBox<String> roomChoice;
+    @FXML
+    private TextField roomNum;
 
+    @FXML
+    public void initialize() {
+        rooms = MongoOps.queryAllByType("Standard");
+        assert rooms != null;
+        ObservableList<Room> obList = FXCollections.observableArrayList(rooms);
+        roomIdCol.setCellValueFactory(new PropertyValueFactory<Room, String>("id"));
+        roomNumCol.setCellValueFactory(new PropertyValueFactory<Room, Integer>("roomNum"));
+        roomOccCol.setCellValueFactory(new PropertyValueFactory<Room, Boolean>("occupied"));
+
+        table.getItems().addAll(rooms);
+    }
+
+    @FXML
+    private void showAddDialogue(ActionEvent event) throws IOException {
+        Stage addStage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        loader = new FXMLLoader(getClass().getResource("AddRoomScene.fxml"));
+        root = loader.load();
+        stage = new Stage();
+        scene = new Scene(root, 320, 365);
+        stage.setScene(scene);
+        stage.show();
+        stage.setResizable(false);
     }
 
     @FXML

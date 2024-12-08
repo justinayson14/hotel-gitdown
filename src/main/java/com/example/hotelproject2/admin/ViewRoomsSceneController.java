57fdade3edb7628f7a8cfb233989aa33e85a72c6
Justin Ayson <121000453+justinayson14@users.noreply.github.com>
2024-12-08 01:28:56 -0800

    Defined methods to add, delete, & load rooms by room type

--- src/main/java/com/example/hotelproject2/admin/ViewRoomsSceneController.java
+++ src/main/java/com/example/hotelproject2/admin/ViewRoomsSceneController.java
@@ -1,9 +1,11 @@
 package com.example.hotelproject2.admin;
 
+import com.example.hotelproject2.HotelController;
 import com.example.hotelproject2.MongoOps;
+import com.example.hotelproject2.models.DeluxeRoom;
+import com.example.hotelproject2.models.PresRoom;
 import com.example.hotelproject2.models.Room;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import com.example.hotelproject2.models.StandardRoom;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -15,6 +17,7 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ViewRoomsSceneController {
@@ -43,11 +46,20 @@ public class ViewRoomsSceneController {
     private ChoiceBox<String> roomChoice;
     @FXML
     private TextField roomNum;
+    @FXML
+    private Label errorText;
+
+    private ToggleGroup roomPicks;
+    private String curRoomType;
 
     @FXML
-    public void initialize() {
-        rooms = MongoOps.queryAllByType("Standard");
-        ObservableList<Room> obList = FXCollections.observableArrayList(rooms);
+    public void initTable() {
+        roomPicks = new ToggleGroup();
+        stdBtn.setToggleGroup(roomPicks);
+        deluxeBtn.setToggleGroup(roomPicks);
+        presBtn.setToggleGroup(roomPicks);
+        stdBtn.setSelected(true);
+        loadRooms(null);
         roomIdCol.setCellValueFactory(new PropertyValueFactory<Room, String>("id"));
         roomNumCol.setCellValueFactory(new PropertyValueFactory<Room, Integer>("roomNum"));
         roomOccCol.setCellValueFactory(new PropertyValueFactory<Room, Boolean>("occupied"));
@@ -56,10 +68,68 @@ public class ViewRoomsSceneController {
     }
 
     @FXML
+    private void initDiag(TableView<Room> table, String curRoomType) {
+        roomChoice.getItems().addAll("Standard", "Deluxe", "Presidential");
+        roomChoice.setValue(curRoomType);
+        this.table = table;
+    }
+
+    @FXML
+    private void addRoom(ActionEvent event) {
+        HotelController r = new HotelController();
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        boolean isAllFilled = r.validateField(roomNum);
+        if(isAllFilled) {
+            boolean isUniqueRoomNum = MongoOps.checkRoomNum(Integer.parseInt(roomNum.getText()));
+            if(isUniqueRoomNum) {
+                errorText.setStyle(null);
+                switch (roomChoice.getValue()) {
+                    case "Standard" -> MongoOps.insertSingle(new StandardRoom(Integer.parseInt(roomNum.getText())));
+                    case "Deluxe" -> MongoOps.insertSingle(new DeluxeRoom(Integer.parseInt(roomNum.getText())));
+                    case "Presidential" -> MongoOps.insertSingle(new PresRoom(Integer.parseInt(roomNum.getText())));
+                }
+                table.getItems().setAll(MongoOps.queryAllByType(roomChoice.getValue()));
+                stage.close();
+            } else
+                errorText.setText("Room number already exists...");
+        }
+    }
+
+    @FXML
+    private void removeRoom(ActionEvent event) {
+        Room selRoom = table.getSelectionModel().getSelectedItem();
+        MongoOps.removeRoom(selRoom);
+        table.getItems().setAll(MongoOps.queryAllByType(curRoomType));
+    }
+
+    @FXML
+    private void loadRooms(ActionEvent event) {
+        List<Room> room = new ArrayList<>();
+        if(stdBtn.isSelected()) {
+            curRoomType = "Standard";
+            rooms = MongoOps.queryAllByType(curRoomType);
+            deluxeBtn.setSelected(false);
+            presBtn.setSelected(false);
+        } else if(deluxeBtn.isSelected()) {
+            curRoomType = "Deluxe";
+            rooms = MongoOps.queryAllByType(curRoomType);
+            stdBtn.setSelected(false);
+            presBtn.setSelected(false);
+        } else if(presBtn.isSelected()) {
+            curRoomType = "Presidential";
+            rooms = MongoOps.queryAllByType(curRoomType);
+            stdBtn.setSelected(false);
+            deluxeBtn.setSelected(false);
+        }
+        table.getItems().setAll(rooms);
+    }
+
+    @FXML
     private void showAddDialogue(ActionEvent event) throws IOException {
-        Stage addStage = (Stage) ((Node)event.getSource()).getScene().getWindow();
         loader = new FXMLLoader(getClass().getResource("AddRoomScene.fxml"));
         root = loader.load();
+        ViewRoomsSceneController controller = loader.getController();
+        controller.initDiag(table, curRoomType);
         stage = new Stage();
         scene = new Scene(root, 320, 365);
         stage.setScene(scene);
